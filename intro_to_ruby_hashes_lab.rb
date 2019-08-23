fbec1268af17866d830e4dbd1d27c4a67e213690
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-23 21:18:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,6 +17,7 @@ def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly = {
 		 :railroads => {
+			 :piece => 4,
 			 :name => {},
 			 :rent_in_dollars => {}
 		 }
